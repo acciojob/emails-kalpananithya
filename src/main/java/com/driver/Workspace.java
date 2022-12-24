fd15b120b96330a525d283df56a40649634bd065
kalpananithya <k_nithyanand@yahoo.com>
2022-12-24 23:28:35 +0530

    commit

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -4,48 +4,47 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class Workspace extends Gmail{
+public class Workspace extends Gmail {
 
-    private ArrayList<Meeting> calendar=new ArrayList<>(); // Stores all the meetings
+    private ArrayList<Meeting> calendar = new ArrayList<>(); // Stores all the meetings
 
     public Workspace(String emailId) {
         super(emailId);
 
     }
 
-    public void addMeeting(Meeting meeting){
+    public void addMeeting(Meeting meeting) {
 
         calendar.add(meeting);
         //add the meeting to calendar
 
     }
 
-    public int findMaxMeetings(){
-
-        Meeting m=calendar.get(0);
-        int ct=0;
-        LocalTime t1= m.getStartTime();
-        LocalTime t2=m.getEndTime();
-
-        for(int i=1;i<calendar.size()-1;i++)
-        {
-           Meeting temp=calendar.get(i);
-            LocalTime time1= m.getStartTime();
-            LocalTime time2=m.getEndTime();
-
-           if(temp.getStartTime().isAfter(t1)  && temp.getStartTime().isBefore(t2))
-              ct++;
-
-           t1=time1;
-           t2=time2;
+    public int findMaxMeetings() {
 
 
-        }
+        int ct = 1;
+
+        for (int i = 0; i < calendar.size() - 1; i++) {
+
+            Meeting t1 = calendar.get(i);
+
+            for (int j = i; j < calendar.size() - 1; j++) {
 
 
-          return ct++;
 
 
+
+                    Meeting temp2;
+                    temp2 = calendar.get(j);
+
+                    if (temp2.getStartTime().isAfter(t1.getStartTime()) && temp2.getEndTime().isAfter(t1.getEndTime()))
+
+                        ct++;
+
+
+
+            }
         }
 
 
@@ -54,7 +53,8 @@ public class Workspace extends Gmail{
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
 
+        return (ct);
+
+
     }
-
-
-
+}
